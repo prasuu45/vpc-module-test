8dcdbb1d7814d22936d204cc8a151caf8a7c1e10
prasuu45 <prasuuchow93@gmail.com>
2024-09-25 14:15:39 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
     value = module.vpc.vpc_id
   
 }
+
+output "az_info" {
+    value = module.vpc.az_info
+  
+}
--- vpc.tf
+++ vpc.tf
@@ -3,5 +3,8 @@ module "vpc" {
     project_name =  var.project_name
     environment =  var.environment
     common_tags = var.common_tags
+    public-subnet_cidrs = var.public_subnet_cidrs
+    private-subnet_cidrs = var.private_subnet_cidrs
+    database-subnet_cidrs = var.database_subnet_cidrs
   
 }
